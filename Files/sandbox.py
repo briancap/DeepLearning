6cbe679139e56c0c7315187d9e2006e884b4e1b5
Brian Cap <bcap@columninfosec.com>
2017-09-17 20:48:34 -0400

    fix multi layer in

--- Files/sandbox.py
+++ Files/sandbox.py
@@ -3,4 +3,15 @@ import numpy as np
 
 df = pd.DataFrame(np.random.randn(8, 4), columns=['A', 'B', 'C', 'D'])
 
-print df[:-2, :]
+def plus_one(x):
+	return x*5
+
+print df[:1]
+print plus_one(df[:1])
+
+'''
+1) sigmoid
+2) forward pass in the train method
+3) backpropin train methos
+4) forward pass in run method
+'''
